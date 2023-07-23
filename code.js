41ad835088e35248a1dfe95102dcd341b924c206
AleV <alejavanegas15@gmail.com>
2023-07-23 09:02:54 -0500

    Solución de errores con el botón

--- code.js
+++ code.js
@@ -1,8 +1,12 @@
 function toggleSection() {
 	var section = document.getElementById("cv-english");
+	var section2 = document.getElementById("cv-spanish");
+
 	if (section.style.display === "none") {
 	  section.style.display = "block";
+	  section2.style.display = "none";
 	} else {
 	  section.style.display = "none";
+	  section2.style.display = "block";
 	}
   }
